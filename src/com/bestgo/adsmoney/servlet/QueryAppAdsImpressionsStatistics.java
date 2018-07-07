65142d7d29d68dcc0056bde10be269becf4456cb
zmjjobs <dream98job@126.com>
2018-07-07 17:06:39 +0800

    页面增加获取全部应用和国家的数据查询字段

--- src/com/bestgo/adsmoney/servlet/QueryAppAdsImpressionsStatistics.java
+++ src/com/bestgo/adsmoney/servlet/QueryAppAdsImpressionsStatistics.java
@@ -35,8 +35,8 @@ public class QueryAppAdsImpressionsStatistics extends HttpServlet {
         try {
             String sql = "SELECT event_date,sum(revenue) AS total_revenue FROM app_ads_impressions_statistics " +
                     "WHERE installed_date = '" + date + "' " +
-                    (null == appId || appId.isEmpty() ? " " : "AND app_id = '" + appId + "' ") +
-                    (null == countryCode || countryCode.isEmpty() ? " " : "AND country_code = '" + countryCode + "' ") +
+                    (appId.isEmpty() ? " " : "AND app_id = '" + appId + "' ") +
+                    (countryCode.isEmpty() ? " " : "AND country_code = '" + countryCode + "' ") +
                     "GROUP BY event_date ORDER BY event_date";
             List<JSObject> revenueList = DB.findListBySql(sql);
             JsonArray array1 = new JsonArray();
