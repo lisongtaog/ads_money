673c9be5a1f6175cef087da88a59ec49b7939852
zmjjobs <dream98job@126.com>
2018-07-10 20:58:23 +0800

    两个表关联有时候会出现错误数据，需要验证

--- src/com/bestgo/adsmoney/servlet/QueryAppAdsRevenueStatistics.java
+++ src/com/bestgo/adsmoney/servlet/QueryAppAdsRevenueStatistics.java
@@ -34,27 +34,36 @@ public class QueryAppAdsRevenueStatistics extends HttpServlet {
         JsonObject json = new JsonObject();
         try {
             //计算实际花费，总安装，购买安装，购买安装在总安装（购买安装+自然量）中的占比
-            String sql = "SELECT SUM(b.spend) AS total_spend,SUM(a.installed) AS total_installed,SUM(b.installed) AS purchase_installed " +
-                    "FROM app_firebase_daily_metrics_history a,app_ads_daily_metrics_history b " +
-                    "WHERE a.date = b.date AND a.app_id = b.app_id AND a.country_code = b.country_code " +
-                    "AND a.date = '" + date + "' " +
-                    ("all".equals(appId) || appId.isEmpty() ? " " : "AND a.app_id = '" + appId + "' ") +
-                    ("all".equals(countryCode) || countryCode.isEmpty() ? " " : "AND a.country_code = '" + countryCode + "' ");
-            JSObject one = DB.findOneBySql(sql);
+            double totalInstalled = 0;
             double actualInstallationRatio = 0;
             double totalSpend = 0;
-            double totalInstalled = 0;
             double purchaseInstalled = 0;
+            String sql = "SELECT SUM(installed) AS total_installed " +
+                    "FROM app_firebase_daily_metrics_history " +
+                    "WHERE date = '" + date + "' " +
+                    ("all".equals(appId) || appId.isEmpty() ? " " : "AND app_id = '" + appId + "' ") +
+                    ("all".equals(countryCode) || countryCode.isEmpty() ? " " : "AND country_code = '" + countryCode + "' ");
+            JSObject one = DB.findOneBySql(sql);
+            if (one.hasObjectData()) {
+                totalInstalled = NumberUtil.convertDouble(one.get("total_installed"),0);
+            }
+            sql = "SELECT SUM(spend) AS total_spend,SUM(installed) AS purchase_installed " +
+                    "FROM app_ads_daily_metrics_history " +
+                    "WHERE date = '" + date + "' " +
+                    ("all".equals(appId) || appId.isEmpty() ? " " : "AND app_id = '" + appId + "' ") +
+                    ("all".equals(countryCode) || countryCode.isEmpty() ? " " : "AND country_code = '" + countryCode + "' ");
+            one = DB.findOneBySql(sql);
+            if (one.hasObjectData()) {
+                totalSpend = NumberUtil.convertDouble(one.get("total_spend"),0);
+                purchaseInstalled = NumberUtil.convertDouble(one.get("purchase_installed"),0);
+                actualInstallationRatio = totalInstalled == 0 ? 0 : purchaseInstalled / totalInstalled;
+            }
+
             JsonArray dateArray = new JsonArray();
             JsonArray dataArray = new JsonArray();
             JsonArray dataTable = new JsonArray();
             JsonArray item = null;
-            if (one.hasObjectData()) {
-                totalSpend = NumberUtil.convertDouble(one.get("total_spend"),0);
-                totalInstalled = NumberUtil.convertDouble(one.get("total_installed"),0);
-                purchaseInstalled = NumberUtil.convertDouble(one.get("purchase_installed"),0);
-                actualInstallationRatio = totalInstalled == 0 ? 0 : purchaseInstalled / totalInstalled;
-            }
+
             //计算在某个安装日期内（的某个应用在某个国家中）的每展示日期总收入
             sql = "SELECT event_date,sum(revenue) AS total_revenue FROM app_ads_impressions_statistics " +
                     "WHERE installed_date = '" + date + "' " +
