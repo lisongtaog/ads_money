870faa7386827b257c4c244c73b28472e631ee01
zhumengjun <dream98job@gmail.com>
2018-08-06 10:58:13 +0800

    修改 查询购买用户每日收入 的接口

--- src/com/bestgo/adsmoney/servlet/InterQueryBuyUserDailyRevenue.java
+++ src/com/bestgo/adsmoney/servlet/InterQueryBuyUserDailyRevenue.java
@@ -1,7 +1,6 @@
 package com.bestgo.adsmoney.servlet;
 
 import com.bestgo.adsmoney.utils.DateUtil;
-import com.bestgo.adsmoney.utils.Utils;
 import com.bestgo.common.database.services.DB;
 import com.bestgo.common.database.utils.JSObject;
 import com.google.gson.JsonArray;
@@ -13,15 +12,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 查询购买用户每天的累计收入 接口
  */
 @WebServlet(name = "InterQueryBuyUserDailyRevenue", urlPatterns = "/query_buy_user_daily_revenue")
 public class InterQueryBuyUserDailyRevenue extends HttpServlet {
+    class RevenueItem {
+        public double first_day_revenue;
+        public double second_day_revenue;
+        public double third_day_revenue;
+        public double fourth_day_revenue;
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String token = request.getParameter("token");
         String installDate = request.getParameter("date");
@@ -29,41 +34,62 @@ public class InterQueryBuyUserDailyRevenue extends HttpServlet {
         JsonArray jsonArray = new JsonArray();
         if ("iLoveMoney".equals(token)) {
             try {
-                    for (int i =0;i<5;i++){
-                        String currInstallDate = DateUtil.addDay(installDate,-i,"yyyy-MM-dd");
-                        String secondDay = DateUtil.addDay(currInstallDate,1,"yyyy-MM-dd");
-                        String thirdDay = DateUtil.addDay(currInstallDate,2,"yyyy-MM-dd");
-                        String fourthDay = DateUtil.addDay(currInstallDate,3,"yyyy-MM-dd");
-                        //计算在某个安装日期内（的某个应用在某个国家中）的每展示日期总收入
-                        String sql = "SELECT event_date,app_id,country_code,sum(revenue) AS total_revenue FROM app_ads_buy_impressions_statistics " +
-                                "WHERE installed_date = '" + currInstallDate + "' " +
-                                "and event_date between '" + currInstallDate + "' and '" + installDate +"' " +
-                                " GROUP BY event_date,app_id,country_code order by event_date";
-                        List<JSObject> revenueList = DB.findListBySql(sql);
+                //key=installDate:appId:countryCode,value=RevenueItem
+                HashMap<String, RevenueItem> map = new HashMap<>();
+                RevenueItem revenueItem = null;
+                String key = null;
+                for (int i =0;i<5;i++){
+                    String currInstallDate = DateUtil.addDay(installDate,-i,"yyyy-MM-dd");
+                    String secondDay = DateUtil.addDay(currInstallDate,1,"yyyy-MM-dd");
+                    String thirdDay = DateUtil.addDay(currInstallDate,2,"yyyy-MM-dd");
+                    String fourthDay = DateUtil.addDay(currInstallDate,3,"yyyy-MM-dd");
+                    System.out.println(currInstallDate + "," + secondDay + "," + thirdDay + "," + fourthDay);
+                    //计算在某个安装日期内（的某个应用在某个国家中）的每展示日期总收入
+                    String sql = "SELECT event_date,app_id,country_code,sum(revenue) AS total_revenue FROM app_ads_buy_impressions_statistics " +
+                            "WHERE installed_date = '" + currInstallDate + "' " +
+                            "and event_date >= '" + currInstallDate + "' and event_date <= '" + installDate + "' " +
+                            "GROUP BY event_date,app_id,country_code order by event_date";
+                    List<JSObject> revenueList = DB.findListBySql(sql);
 
-                        for (int j =0;j<revenueList.size();j++){
-                            JSObject js = revenueList.get(j);
-                            String eventDate = js.get("event_date").toString();
-                            String appId = js.get("app_id");
-                            String countryCode = js.get("country_code");
-                            double totalRevenue = js.get("total_revenue");
-                            JsonObject one = new JsonObject();
-                            one.addProperty("intall_date",currInstallDate);
-                            one.addProperty("app_id",appId);
-                            one.addProperty("country_code",countryCode);
-                            if (currInstallDate.equals(eventDate)) {
-                                one.addProperty("first_day_revenue",totalRevenue);
-                            }else if (secondDay.equals(eventDate)) {
-                                one.addProperty("second_day_revenue",totalRevenue);
-                            }else if (thirdDay.equals(eventDate)) {
-                                one.addProperty("third_day_revenue",totalRevenue);
-                            }else if (fourthDay.equals(eventDate)) {
-                                one.addProperty("fourth_day_revenue",totalRevenue);
-                            }
-                            jsonArray.add(one);
+                    for (int j =0;j<revenueList.size();j++){
+                        JSObject js = revenueList.get(j);
+                        String eventDate = js.get("event_date").toString();
+                        String appId = js.get("app_id");
+                        String countryCode = js.get("country_code");
+                        double totalRevenue = js.get("total_revenue");
+                        key = currInstallDate + ":" + appId + ":" + countryCode;
+                        revenueItem = map.get(key);
+                        if (revenueItem == null) {
+                            revenueItem = new RevenueItem();
                         }
+                        if (currInstallDate.equals(eventDate)) {
+                            revenueItem.first_day_revenue = totalRevenue;
+                        }else if (secondDay.equals(eventDate)) {
+                            revenueItem.second_day_revenue = totalRevenue;
+                        }else if (thirdDay.equals(eventDate)) {
+                            revenueItem.third_day_revenue = totalRevenue;
+                        }else if (fourthDay.equals(eventDate)) {
+                            revenueItem.fourth_day_revenue = totalRevenue;
+                        }
+                        map.put(key,revenueItem);
                     }
-
+                }
+                if (map.size() > 0) {
+                    for (Map.Entry<String,RevenueItem> entry : map.entrySet()) {
+                        JsonObject js = new JsonObject();
+                        String currKey = entry.getKey();
+                        String[] split = currKey.split(":");
+                        RevenueItem item = entry.getValue();
+                        js.addProperty("install_date",split[0]);
+                        js.addProperty("app_id",split[1]);
+                        js.addProperty("country_code",split[2]);
+                        js.addProperty("first_day_revenue",item.first_day_revenue);
+                        js.addProperty("second_day_revenue",item.second_day_revenue);
+                        js.addProperty("third_day_revenue",item.third_day_revenue);
+                        js.addProperty("fourth_day_revenue",item.fourth_day_revenue);
+                        jsonArray.add(js);
+                    }
+                }
                 response.getWriter().write(jsonArray.toString());
             } catch (Exception ex) {
                 ex.printStackTrace();
