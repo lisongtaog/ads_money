4a9e2dbb9a6636081489c82ac7b46a119ba58128
zhumengjun <dream98job@126.com>
2018-10-23 10:25:50 +0800

    每日更新收入的历史表插入更改SQL

--- src/main/java/com/bestgo/adsmoney/servlet/UpdateDailyMetrics.java
+++ src/main/java/com/bestgo/adsmoney/servlet/UpdateDailyMetrics.java
@@ -1,6 +1,7 @@
 package com.bestgo.adsmoney.servlet;
 
 import com.bestgo.adsmoney.cache.GlobalCache;
+import com.bestgo.adsmoney.utils.DateUtil;
 import com.bestgo.common.database.services.DB;
 
 import javax.servlet.ServletException;
@@ -18,14 +19,19 @@ public class UpdateDailyMetrics extends HttpServlet {//供money_tools调用使�
         if ("iLoveMoney".equals(token)) {
             try {
                 DB.delete("app_daily_metrics_history").where(DB.filter().whereEqualTo("date", date)).execute();
-                String sql = "insert into app_daily_metrics_history select date, app_id, ad_network, country_code, sum(ad_request) as ad_request, sum(ad_filled) as ad_filled, " +
+                String sql = "insert into app_daily_metrics_history(date,app_id,ad_network,country_code,ad_request,ad_filled,ad_impression,ad_click,ad_revenue,ad_new_revenue) select date, app_id, ad_network, country_code, sum(ad_request) as ad_request, sum(ad_filled) as ad_filled, " +
                         "sum(ad_impression) as ad_impression, sum(ad_click) as ad_click, sum(ad_revenue) as ad_revenue,"+
                         "(SELECT SUM(IFNULL(s.ad_revenue,0)) FROM app_ad_unit_metrics_history s WHERE s.date = d.date "+
                         "   AND s.app_id = d.app_id AND s.ad_network = d.ad_network AND s.country_code = d.country_code "+
                         "   AND s.ad_unit_id IN (SELECT ad_unit_id from app_ad_unit_config WHERE flag = '1' ) "+
-                        ")AS ad_new_revenue,null,null "+
+                        ")AS ad_new_revenue "+
                         " from app_ad_unit_metrics_history d " +
                         " where date = '" + date + "' group by date, app_id, ad_network, country_code";
+                DB.updateBySql(sql);
+                String yesterday = DateUtil.addDay(date,-1,"yyyy-MM-dd");
+                sql = "UPDATE app_daily_metrics_history a,\n" +
+                        "(SELECT app_id,country_code,ad_network,sum_ad_revenue FROM app_daily_metrics_history WHERE date = '"+yesterday+"' GROUP BY app_id,country_code,ad_network) b\n" +
+                        "SET a.sum_ad_revenue = a.ad_revenue + b.sum_ad_revenue WHERE a.date = '"+date+"' AND a.app_id = b.app_id AND a.country_code = b.country_code AND a.ad_network = b.ad_network";
                 DB.updateBySql(sql);
                 response.getWriter().write("ok");
             } catch (Exception e) {
